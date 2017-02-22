dade45c9190db540ba03ca13fc0b1b293820d0eb
Kandula, Sandeep <Sandeep.Kandula@ampf.com>
2017-02-22 08:37:49 -0600

    added monkey sample project

---
+++ samplePrj/PBDataTableView/PBDataTable/Classes/PBDataTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  PBDataTableViewCell.swift
+//  PBDataTable
+//
+//  Created by praveen b on 6/10/16.
+//  Copyright © 2016 Praveen. All rights reserved.
+//
+
+import UIKit
+
+class PBDataTableViewCell: UITableViewCell {
+    var columnWidth: CGFloat = 80
+
+    @IBOutlet var leftBorderView: UIView!
+    @IBOutlet var rightBorderView: UIView!
+    @IBOutlet var bottomBorderView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        var labelXaxis: CGFloat = 0
+        
+        for i in 0..<ApplicationDelegate.numberOfColumns {
+            
+            let columnLbl = UILabel()
+
+            //If Last Column of the Cell is Button
+            if  ApplicationDelegate.cellLastColumnButtonEnable == true && i == ApplicationDelegate.numberOfColumns-1 {
+                let editBtn = UIButton(type: .Custom)
+                editBtn.frame = CGRectMake(labelXaxis, (CGRectGetHeight(self.frame)-35)/2, 70,35)
+                editBtn.backgroundColor = UIColor(red: 64/255, green: 64/255, blue: 64/255, alpha: 1.0)
+                editBtn.tag = 100+i
+                editBtn.setTitle("Edit", forState: .Normal)
+                self.contentView.addSubview(editBtn)
+            }else {
+                columnLbl.frame = CGRectMake(labelXaxis, 0, columnWidth,CGRectGetHeight(self.frame))
+                columnLbl.textAlignment = .Center
+                columnLbl.font = UIFont(name: "Arial", size: 10)
+                columnLbl.tag = 100+i
+                columnLbl.adjustsFontSizeToFitWidth = true
+                self.contentView.addSubview(columnLbl)
+            }
+            
+            if i != ApplicationDelegate.numberOfColumns-1 {
+                let innerBorderView = UIView()
+                innerBorderView.frame = CGRectMake(CGRectGetMaxX(columnLbl.frame)-1, 0, 1, CGRectGetHeight(self.frame))
+                innerBorderView.backgroundColor = UIColor(red: 64/255, green: 64/255, blue: 64/255, alpha: 1.0)
+                innerBorderView.tag = 200+i
+                self.contentView.addSubview(innerBorderView)
+            }
+            
+            labelXaxis = labelXaxis + columnWidth
+        }
+
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ samplePrj/PBDataTableView/README.md
@@ -0,0 +1,25 @@
+# PBDataTableView
+DataTable for iPhone and iPad
+
+An easy-to-use PBDataTableView with sorting and search option.
+
+## Quick Start
+<br>
+1. Drop PBDataTableView.swift and PBDataTableViewCell.swift with their respective xib files.<br>
+2. Intialize the xib file on your view controller.<br>
+3. Set up the PBDataTableView with frame and customize the search bar.<br>
+4. The Header of the Tableview given to "columnDataArray" with name of the header and the width size of the each header. (If total width of the header exceeds its superview the view will be scrolled horizontally.)<br>
+5. The Row content of the Tableview given to "dataTableArray" as shown in ViewController.<br>
+NOTE* It will be easy if you dont change the dictionay key. If you need to change the Keys, please make the change on "PBDataTableView" too.
+
+![Alt text](https://github.com/praveenb-ios/PBDataTableView/blob/master/iPAD_DataTableView.png "DataTableView_iPad")
+
+## The MIT License (MIT)
+
+Copyright (c) 2016 Praveen B<br>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br><br>
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.<br>
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
